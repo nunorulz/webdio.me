8086034b55833c845c362a2cf83ca839e86c6a08
Bruno <nunorulz@gmail.com>
2023-07-13 10:21:40 -0300

    Novos estudos python

---
+++ MS Investigador/decrypt.py
@@ -0,0 +1,28 @@
+#print( "Hello, Contosoville!" )
+# This is a comment that won't be interpreted as a command.
+
+# Associate variable town with the value "Contosoville"
+#town = "Contosoville"
+
+# Print a message about the secret location
+#print( "The town I am looking for is " + town )
+
+# Define a power (function) to chant a phrase
+#def chant( phrase ):
+    # Glue three copies together and print it as a message
+    #print( phrase + phrase + phrase )
+
+# Invoke the power to chant on the phrase "Contosoville!"
+#chant( "Contosoville!" )
+
+def lasso_letter( letter, shift_amount ):
+    letter_code = ord(letter.lower())
+    decoded_letter_code = letter_code + shift_amount
+    decoded_letter = chr(decoded_letter_code)
+    return decoded_letter
+
+print(lasso_letter('a', 2))
+
+a = float("a")
+
+print (a)
